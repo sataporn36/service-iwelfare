85b5e1b583363d25f1c33677bce95174844ac104
sataporn <satapornsanla36@gmail.com>
2022-12-14 18:07:35 +0700

    first commit

---
+++ src/main/java/com/project/demo/ProjectIwelFareApplication.java
@@ -0,0 +1,13 @@
+package com.project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectIwelFareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectIwelFareApplication.class, args);
+	}
+
+}
